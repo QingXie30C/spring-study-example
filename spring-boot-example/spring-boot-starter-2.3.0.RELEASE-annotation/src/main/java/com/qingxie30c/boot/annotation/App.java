91cc3388fe44e808f3c59293b69d69b23b427aa1
fandong <871119239@qq.com>
2020-06-06 17:24:26 +0800

    ss

--- spring-boot-example/spring-boot-starter-2.3.0.RELEASE-annotation/src/main/java/com/qingxie30c/boot/annotation/App.java
+++ spring-boot-example/spring-boot-starter-2.3.0.RELEASE-annotation/src/main/java/com/qingxie30c/boot/annotation/App.java
@@ -12,6 +12,6 @@ public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class,args);
     }
-
+    //testts2
     private final String name2 ="test1";
 }
