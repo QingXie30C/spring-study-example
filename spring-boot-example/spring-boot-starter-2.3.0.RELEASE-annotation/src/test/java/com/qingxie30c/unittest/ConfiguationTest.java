65ce437347e2f2b439462708b43fe8570ad765c9
qingxie30c <fandong90@yeah.net>
2020-06-06 16:42:04 +0800

    搭建基本使用框架

---
+++ spring-boot-example/spring-boot-starter-2.3.0.RELEASE-annotation/src/test/java/com/qingxie30c/unittest/ConfiguationTest.java
@@ -0,0 +1,12 @@
+package com.qingxie30c.unittest;
+
+import com.qingxie30c.boot.annotation.App;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest(classes = {App.class})
+@RunWith(SpringRunner.class)
+public class ConfiguationTest {
+
+}
